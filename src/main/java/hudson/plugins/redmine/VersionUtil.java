c3cdbb4f871434fb6dbd6080f476cbcdfaddd56f
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2013-07-10 00:40:21 +0900

    define isVersionBefore090, fix condition for other methods

--- src/main/java/hudson/plugins/redmine/VersionUtil.java
+++ src/main/java/hudson/plugins/redmine/VersionUtil.java
@@ -8,13 +8,26 @@ public class VersionUtil {
 		}
 		String[] versions = version.split("\\.");
 		if(versions.length == 3 && 
-		  (Integer.valueOf(versions[0]) == 0) ||		
-		  (Integer.valueOf(versions[0]) == 1 && Integer.valueOf(versions[1]) < 2)) {
+		  ((Integer.valueOf(versions[0]) == 0) ||
+		   (Integer.valueOf(versions[0]) == 1 && Integer.valueOf(versions[1]) < 2))) {
 			return true;
 		}
 		return false;
 	}
 	
+	public static boolean isVersionBefore090(String version) {
+		if(version == null || version.isEmpty()) {
+			return false; // null is redmine latest version 
+		}
+		String[] versions = version.split("\\.");
+		if(versions.length == 3 && 
+		  Integer.valueOf(versions[0]) == 0 && 
+		  Integer.valueOf(versions[1]) < 9) {
+			return true;
+		}
+		return false;
+	}
+
 	public static boolean isVersionBefore081(String version) {
 		if(version == null || version.isEmpty()) {
 			return false; // null is redmine latest version 
@@ -22,8 +35,8 @@ public class VersionUtil {
 		String[] versions = version.split("\\.");
 		if(versions.length == 3 && 
 		  Integer.valueOf(versions[0]) == 0 && 
-		  ( Integer.valueOf(versions[1]) < 8 ) ||
-		  ( Integer.valueOf(versions[1]) == 8  && Integer.valueOf(versions[2]) <  1)) {
+		  ((Integer.valueOf(versions[1]) < 8) ||
+		   (Integer.valueOf(versions[1]) == 8  && Integer.valueOf(versions[2]) < 1))) {
 			return true;
 		}
 		return false;
